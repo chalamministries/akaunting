18962f7a558af5bf11dd474f72b7fd5aedbad637
chalamministries <genspark_dev@genspark.ai>
2026-02-18 17:07:52 +0000

    chore(fluidpay): move tokenizer script to asset file

---
+++ modules/FluidPay/webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.options({
+        terser: {
+            extractComments: false,
+        }
+    })
+    .js('Resources/assets/js/vendor/fluidpay.js', 'Resources/assets/js/fluidpay.min.js')
+    // .sass('./../../resources/assets/sass/argon.scss', './../../public/css')
+    .vue();
